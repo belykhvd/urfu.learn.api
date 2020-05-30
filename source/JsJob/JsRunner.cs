9dd51ca33b34d7ccddc5aaf26bff3117b84a1ef8
belykhvd <belykhvd@gmail.com>
2020-05-30 05:08:29 +0500

    JsRunner

---
+++ source/JsJob/JsRunner.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Jint;
+using Jint.Parser;
+using Jint.Runtime;
+
+namespace JsJob
+{
+    public class JsRunner
+    {
+        public object Execute(string source, object input = null)
+        {
+            try
+            {
+                var engine = new Engine(options =>
+                {
+                    options.DiscardGlobal();
+                    options.LimitRecursion(20);
+                    options.TimeoutInterval(TimeSpan.FromSeconds(10));
+                    options.MaxStatements(200);
+                });
+
+                return engine
+                    .SetValue("input", input)
+                    .Execute(source)
+                    .GetCompletionValue()
+                    .ToObject();
+            }
+            catch (ParserException p)
+            {
+                return $"Compilation error: {p.Message}";
+            }
+            catch (JavaScriptException js)
+            {
+                return $"Runtime error: {js.Message}";
+            }
+            catch (TimeoutException)
+            {
+                return "Time limit exceeded";
+            }
+            catch (RecursionDepthOverflowException)
+            {
+                return "Recursion limit exceeded";
+            }
+            catch (StatementsCountOverflowException)
+            {
+                return "Statements limit exceeded";
+            }
+        }
+    }
+}
---
+++ source/JsJob/JsRunnerTests.cs
@@ -0,0 +1,78 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace JsJob
+{
+    [TestFixture]
+    public class JsRunnerTests
+    {
+        private readonly JsRunner runner = new JsRunner();        
+
+        [Test]
+        public void Return1Plus1()
+        {
+            runner.Execute("1 + 1").Should().Be(2);
+        }
+
+        [Test]
+        public void TestLoop()
+        {
+            runner.Execute(
+                @"var x = 0;
+                  for (var i = 0; i < 2; i++) {
+                      x += 2;
+                  }
+                  x"
+            ).Should().Be(4);
+        }
+
+        [Test]
+        public void TestConditions()
+        {
+            runner.Execute(
+                @"var x = 0;
+                  if (x === 0) 5;
+                  else 10;"
+            ).Should().Be(5);
+        }
+
+        [Test]
+        public void TestInput()
+        {
+            runner.Execute("input", 42).Should().Be(42);
+        }
+
+        [Test]
+        public void TestJsException()
+        {
+            runner.Execute("throw 'Unexpected'").Should().Be("Runtime error: Unexpected");
+        }
+
+        [Test]
+        public void TestParserException()
+        {
+            runner.Execute("0df = 1").Should().Be("Compilation error: Line 1: Unexpected token ILLEGAL");
+        }
+
+        [Test]
+        public void TestTimeLimitExceeded()
+        {
+            runner.Execute("while (true) {}").Should().Be("Time limit exceeded");
+        }
+
+        //[Test]
+        public void TestMemoryLimitExceeded()
+        {
+            runner.Execute("var a = new Array(1000000)").Should().Be("Memory limit exceeded");
+        }
+
+        [Test]
+        public void TestRecursionLimitExceeded()
+        {
+            runner.Execute(
+                @"function r() { r(); }
+                  r();"
+            ).Should().Be("Recursion limit exceeded");
+        }
+    }
+}
